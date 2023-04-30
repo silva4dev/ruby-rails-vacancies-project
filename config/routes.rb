27910165393e90552d8cfedfdbb39c7fbcd36329
silva4dev <web4dev.lucas@outlook.com>
2023-04-30 06:46:34 -0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  root "welcome#index"
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
+
+  get "jobs/details/:id", to: "jobs#show", as: "jobs_details"
+
+  root "jobs#index"
 end
