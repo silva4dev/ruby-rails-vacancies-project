ee9a193837ec02cdc36d40fcc2c76b7dff6253ae
silva4dev <web4dev.lucas@outlook.com>
2023-05-01 03:33:47 -0300

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'must be a valid email address' }
+  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: :bad_email },
+                    uniqueness: { case_sensitive: false }
 end
