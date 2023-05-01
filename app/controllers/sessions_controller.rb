83799d748da90c79674494bcc1827b050e3446b8
silva4dev <web4dev.lucas@outlook.com>
2023-05-01 03:32:50 -0300

    Update sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,23 @@
 class SessionsController < ApplicationController
+  before_action :require_user_not_logged_in!, only: %i[new create]
+  after_action :require_flash_session!, only: %i[create]
+
   def new
   end
 
   def create
-    user = User.find_by(email: params[:email])
-    if user.present? && user.authenticate(params[:password])
+    user = User.find_by(email: params[:user][:email])
+    if user.present? && user.authenticate(params[:user][:password])
       session[:user_id] = user.id
-      redirect_to root_path, notice: "Logged in successfully"
+      redirect_to root_path, notice: I18n.t("flash_sessions_notice")
     else
-      flash[:alert] = "Invalid email or password"
+      flash[:alert] = I18n.t("flash_sessions_invalid")
       render :new
     end
-    flash.clear
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: "Logged out"
+    redirect_to root_path, notice: I18n.t("flash_sessions_logout")
   end
 end
